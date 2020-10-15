b6eea3a442c714bd1ace3d5225aa5658dc76cf85
cceni03 <czc5230.cc@gmail.com>
2020-10-15 09:18:47 -0400

    Update

--- Cristina/static.js/js/logic.js
+++ Cristina/static.js/js/logic.js
@@ -10,7 +10,7 @@ var myMap = L.map("map", {
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
-    accessToken: "pk.eyJ1IjoiY2NlbmkwMyIsImEiOiJja2ZxNTZ1bWEyNWViMnJ0NGF3NjVqM3FoIn0._NFujpCBvSkVHsXyhRcSxQ"
+    accessToken: API_Key
   }).addTo(myMap);
 
   // Country Data
@@ -89,6 +89,37 @@ var countries = [
 
 // Set markerSize function
 
+var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+    denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
+    aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
+    golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
 
+
+    var cities = L.layerGroup([littleton, denver, aurora, golden]);
+
+    var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution}),
+    streets   = L.tileLayer(mapboxUrl, {id: 'MapID', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution});
+
+var map = L.map('map', {
+    center: [39.73, -104.99],
+    zoom: 10,
+    layers: [grayscale, cities]
+});
+
+var baseMaps = {
+  "Grayscale": grayscale,
+  "Streets": streets
+};
+
+var overlayMaps = {
+  "Cities": cities
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+var baseMaps = {
+  "<span style='color: gray'>Grayscale</span>": grayscale,
+  "Streets": streets
+};
 
 
