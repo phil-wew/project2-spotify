4cbc5099bc4f5c8710a89fdd86f1534a4c19b400
SOathout <scottoathout1982@gmail.com>
2020-10-18 18:40:11 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -8,27 +8,41 @@ Object.values(tNames).forEach((tName) => {
     .property("value", tName)
             })
 
-// var genres = {
-//     x: streams_ids,
-//     y: sample_values,
-//     text: region_labels,
-//     mode: 'markers',
-//     marker: {
-//     size: sample_values,
-//     color: streams_ids,
-//     colorscale:"Electric"
+  region_df = px.data.genres_wide()
+  fig = px.bar(region_df, x="Region", y=["gold", "silver", "bronze"], title="Wide-Form Input")
+  fig.show()
+
+// d3.json("/regions").then(function(data) {
+// var selector = d3.select("#selDataset");
+// var samples = data.samples
+// var filterArray = samples.filter(sampleObject => sampleObject.id == sample)
+// var result = filterArray[0]
+// var sample_values = result.sample_values
+// var regions_ids = result.regions_ids
+// var regions_labels = result.regions_labels; 
+
+//     // Bubble Chart
+// var streams = {
+//         x: streams_ids,
+//         y: sample_values,
+//         text: region_labels,
+//         mode: 'markers',
+//         marker: {
+//         size: sample_values,
+//         color: streams_ids,
+//         colorscale:"Electric"
 //         }
 //     };
-        
+
 // var data = [song1];
 // var layout = {
-//     title: 'Most streamed songs',
-//     showlegend: false,
-//     hovermode: 'closest',
-//     xaxis: {title:"Number of streams " +sample},
-//     margin: {t:30}
-//             };
-// Plotly.newPlot('bubble', data, layout); 
+//         title: 'Most streamed songs',
+//         showlegend: false,
+//         hovermode: 'closest',
+//         xaxis: {title:"Number of streams " +sample},
+//         margin: {t:30}
+//     };
+//     Plotly.newPlot('bubble', data, layout); 
 
  console.log(data)
 })
@@ -64,6 +78,3 @@ Object.values(tNames).forEach((tName) => {
 //         margin: {t:30}
 //     };
 //     Plotly.newPlot('bubble', data, layout); 
-
-    // console.log(data)
-    // })
