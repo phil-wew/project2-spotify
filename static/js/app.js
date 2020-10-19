6653c116f7176b1b2fba4c86d19b911e8be967d1
SOathout <scottoathout1982@gmail.com>
2020-10-19 13:07:03 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -27,15 +27,6 @@ tNames.forEach((tName) => {
 
             console.log(data['Region'])
 })
-// var bar = [
-//   {
-//   x: Object.values(data["Region"]),
-//   y: Object.values(data["Streams"]),
-//   type: 'bar'
-//   }
-//   ];
-         
-//   Plotly.newPlot('bar', bar);
     
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
@@ -44,16 +35,8 @@ function optionChanged(newSample) {
 
   console.log(data)
 
-// d3.json("/regions").then(function(regions) {
-// var selector = d3.select("#selDataset");
-
-// var filterArray = charts.filter(chartsObject => chartsObject.id == charts)
-// var result = filterArray[0]
-// var regions_values = result.regions_values
-// var regions_ids = result.regions_ids
-// var regions_labels = result.regions_labels; 
-
 // Bubble Chart
+
 var bubbleLayout = {
   title: "Top Music Genre",
   margin: { t:0 },
@@ -64,26 +47,59 @@ var bubbleLayout = {
 
 var bubbleData = [
   {
-    x: Object.values(data["Regions"]),
+    x: Object.values(data["Region"]),
     y: Object.values(data["Top Genre"]),
     text: regions_labels,
     mode: "markers",
     marker : {
       size: Object.values(data["Top Genre"]),
-      color: Object.values(data["Regions"]),
+      color: Object.values(data["Region"]),
       colorscale: "Earth"
     }
 
   }
 ];
 
-Plotly.newPlot("bubble", bubbleData, bubbleLayout)
+Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
-Plotly.newPlot('myDiv', data);
+Plotly.newPlot('bubble', bubbleData);
 
+// function buildCharts(column){
 
+//   d3.json("/regions").then(function(data) {
+//                   console.log(data['Region'])
+//       var scatter = [
+//         {
+//         x: Object.values(data["Region"]),
+//         y: Object.values(data[column]),
+//         type: 'scatter'
+//         }
+//         ];
+               
+//         Plotly.newPlot('scatter', scatter);
+//   })
+//   }
+//   buildCharts("Streams")
+//   d3.json("/regions").then(function(data) {
+//   var selector = d3.select("#selDataset");
+//   tNames = Object.keys(data)
+//   console.log(tNames)
+//   tNames.forEach((tName) => {
+//     selector
+//       .append("option")
+//       .text(tName)
+//       .property("value", tName)
+//               })
+  
+//               console.log(data['Region'])
+//   })
 
-
+//   function optionChanged(newSample) {
+//     // Fetch new data each time a new sample is selected
+//     buildCharts(newSample);
+//   }
+  
+    console.log(data)
 
     
 
