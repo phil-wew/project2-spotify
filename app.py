b66f586a55f1b1e1aeb6f10a2e85ce94b3d97138
phil-wew <pwewiora08@Gmail.com>
2020-10-19 18:47:58 -0700

    map route edits

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/project2-spotify/sabrina/index.html')
+@app.route('map')
 def map_index():
     return render_template("index2.html")
 
