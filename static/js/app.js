4af27f9fd2fa66310b4ac1f56dc8681afd2f0c1c
phil-wew <pwewiora08@Gmail.com>
2020-10-20 15:00:57 -0700

    app.py

--- static/js/app.js
+++ static/js/app.js
@@ -1,75 +1,62 @@
 // function init() {
 
-function buildCharts(column){
+function buildCharts(column) {
 
-d3.json("/regions").then(function(data) {
-                console.log(data['Region'])
-    var bar = [
-      {
-      x: Object.values(data["Region"]),
-      y: Object.values(data[column]),
-      type: 'bar'
-      }
-      ];
-    var layout = {
-      title: 'Bar Chart',
-      xaxis: {
-         automargin: true
-      }
-     };
-      
-      Plotly.newPlot('bar', bar, layout);
-})
+    d3.json("/regions").then(function(data) {
+        console.log(data['Region'])
+        var bar = [{
+            x: Object.values(data["Region"]),
+            y: Object.values(data[column]),
+            type: 'bar'
+        }];
+        var layout = {
+            title: 'Regional Comparison',
+            xaxis: {
+                automargin: true
+            }
+        };
+
+        Plotly.newPlot('bar', bar, layout);
+    })
 }
 buildCharts("Streams")
 d3.json("/regions").then(function(data) {
-var selector = d3.select("#selDataset");
-tNames = Object.keys(data)
-console.log(tNames)
-tNames.forEach((tName) => {
-  selector
-    .append("option")
-    .text(tName)
-    .property("value", tName)
-            })
+    var selector = d3.select("#selDataset");
+    tNames = Object.keys(data)
+    console.log(tNames)
+    tNames.forEach((tName) => {
+        selector
+            .append("option")
+            .text(tName)
+            .property("value", tName)
+    })
 
-            console.log(data['Region'])
+    console.log(data['Region'])
 })
 
 
 function optionChanged(newSample) {
-        // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-      }
-      
-     
-  d3.json("/regions").then(function(data) {
-    console.log(data)
-  
-  // Bubble Chart
-  var bubbleLayout = {
-    title: "Top Music Genre",
-    margin: { t:0 },
-    hovermode: "closest",
-    xaxis: { title: "Regions"},
-    yaxis: { title: "Top Genre Popularity"},
-    margin: {t:30},
-    xaxis: {
-      automargin: true}
-  };
-  var bubbleData = [
-    {
-      x: Object.values(data["Region"]),
-      y: Object.values(data["popularity"]),
-      text: Object.values(data["topgenre"]),
-      mode: "markers",
-      marker : {
-        size: Object.values(data["popularity"]),
-        // size: Object.values(data["topgenre"]),
-        color: Object.values(data["popularity"]),
-        colorscale: "Earth",
-      }
-    }
-  ];
-  Plotly.newPlot("bubble", bubbleData, bubbleLayout)
-  });
+    // Fetch new data each time a new sample is selected
+    buildCharts(newSample);
+}
+
+   
+d3.json("/regions").then(function(data)  {  
+    console.log(data)
+
+    // Bubble Chart
+    var  bubbleLayout  =   {  
+        title:   "Top Music Genre",
+          margin:  {  t: 0  },
+          hovermode:   "closest",
+          xaxis:  {  title:   "Regions" },
+        yaxis: { title: "Top Genre Popularity" },
+          margin:  { t: 30 },
+        xaxis: {
+            automargin: true
+        }
+    };
+    var  bubbleData  =   [  {     x:  Object.values(data["Region"]),     y:  Object.values(data["popularity"]),     text:  Object.values(data["topgenre"]),     mode:   "markers",     marker :  {       size:  Object.values(data["popularity"]),        // size: Object.values(data["topgenre"]),
+                  color:  Object.values(data["popularity"]),       colorscale:   "Earth",      }   }];
+    Plotly.newPlot("bubble",  bubbleData,  bubbleLayout)
+});
