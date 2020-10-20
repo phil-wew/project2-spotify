31eb99ae50f5df49cf311068bd95c760c8551783
phil-wew <pwewiora08@Gmail.com>
2020-10-19 18:45:59 -0700

    adding app.py route for map

--- app.py
+++ app.py
@@ -39,35 +39,40 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/project2-spotify/sabrina/index.html')
+def map_index():
+    return render_template("index2.html")
+
+
 @app.route("/data")
 def data_query():
 
     dictionary = {"Position": df['Position'].tolist(), "Track Name": df['Track Name'].tolist(), "Artist": df['Artist'].tolist(), "Stream": df['Streams'].tolist(),
-                "Week": df['Week'].tolist(), "Region": df['Region'].tolist(), "Acousticness": df['acousticness'].tolist(), "Danceability": df['danceability'].tolist(),
+                  "Week": df['Week'].tolist(), "Region": df['Region'].tolist(), "Acousticness": df['acousticness'].tolist(), "Danceability": df['danceability'].tolist(),
                   "Duration": df['duration_ms'].tolist(), "Energy": df['energy'].tolist(), "Explicit": df['explicit'].tolist(), "ID": df['id'].tolist(),
                   "Instrumentalness": df['instrumentalness'].tolist(), "Key": df['key'].tolist(), "Liveness": df['liveness'].tolist(),
                   "Loudness": df['loudness'].tolist(), "Mode": df['mode'].tolist(), "Popularity": df['popularity'].tolist(), "Release Date": df['release_date'].tolist(),
-                  "Speechiness": df['speechiness'].tolist(), "Tempo": df['tempo'].tolist(), "Valence": df['valence'].tolist(), "Top Genre" : df['top genre']}
+                  "Speechiness": df['speechiness'].tolist(), "Tempo": df['tempo'].tolist(), "Valence": df['valence'].tolist(), "Top Genre": df['top genre']}
 
-    
     return df.to_json(orient='columns')
+
 
 @app.route('/regions')
 def regions():
 
-    region_dict = {"Track Name": region_df['TrackName'].tolist(), "Artist": region_df['Artist'].tolist(), 
-                  "Stream": region_df['Streams'].tolist(), "Region": region_df['Region'].tolist(), 
-                  "Acousticness": region_df['acousticness'].tolist(), "Danceability": region_df['danceability'].tolist(),
-                  "Duration": region_df['duration_ms'].tolist(), "Energy": region_df['energy'].tolist(),
-                  "Instrumentalness": region_df['instrumentalness'].tolist(), 
-                  "Key": region_df['key'].tolist(), "Liveness": region_df['liveness'].tolist(),
-                  "Loudness": region_df['loudness'].tolist(), "Mode": region_df['mode'].tolist(), 
-                  "Popularity": region_df['popularity'].tolist(),
-                  "Speechiness": region_df['speechiness'].tolist(), "Tempo": region_df['tempo'].tolist(),
-                   "Valence": region_df['valence'].tolist(), "Top Genre" : region_df['topgenre']}
-
+    region_dict = {"Track Name": region_df['TrackName'].tolist(), "Artist": region_df['Artist'].tolist(),
+                   "Stream": region_df['Streams'].tolist(), "Region": region_df['Region'].tolist(),
+                   "Acousticness": region_df['acousticness'].tolist(), "Danceability": region_df['danceability'].tolist(),
+                   "Duration": region_df['duration_ms'].tolist(), "Energy": region_df['energy'].tolist(),
+                   "Instrumentalness": region_df['instrumentalness'].tolist(),
+                   "Key": region_df['key'].tolist(), "Liveness": region_df['liveness'].tolist(),
+                   "Loudness": region_df['loudness'].tolist(), "Mode": region_df['mode'].tolist(),
+                   "Popularity": region_df['popularity'].tolist(),
+                   "Speechiness": region_df['speechiness'].tolist(), "Tempo": region_df['tempo'].tolist(),
+                   "Valence": region_df['valence'].tolist(), "Top Genre": region_df['topgenre']}
 
     return region_df.to_json(orient='columns')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
